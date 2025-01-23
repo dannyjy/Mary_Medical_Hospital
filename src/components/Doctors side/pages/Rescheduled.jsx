2392a596000381c9a5a13908d9421e517562861c
Danny <d.juzu7777@gmail.com>
2025-01-23 15:50:34 +0200

    Commented the reschduled section

--- src/components/Doctors side/pages/Rescheduled.jsx
+++ src/components/Doctors side/pages/Rescheduled.jsx
@@ -1,6 +1,6 @@
 import AppointmentHeader from "../../../ui/AppointmentHeader.jsx";
-import {BookAppointmentData} from "../doctorData.js";
-import AppointmentCard from "../../../ui/AppointmentCard.jsx";
+// import {BookAppointmentData} from "../doctorData.js";
+// import AppointmentCard from "../../../ui/AppointmentCard .jsx";
 
 const Rescheduled = () => {
     return (
@@ -8,16 +8,16 @@ const Rescheduled = () => {
             <AppointmentHeader heading="Rescheduled Appointments"/>
             <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
                 {
-                    BookAppointmentData.map((appoinitment,index) =>(
-                        <AppointmentCard key={index}
-                             name={appoinitment.Name}
-                             date={appoinitment.date}
-                             time={appoinitment.time}
-                             description={appoinitment.Description}
-                             age={appoinitment.Age}
-                             contact={appoinitment.contact}
-                        />
-                    ))
+                    // BookAppointmentData.map((appoinitment,index) =>(
+                    //     <AppointmentCard key={index}
+                    //          name={appoinitment.Name}
+                    //          date={appoinitment.date}
+                    //          time={appoinitment.time}
+                    //          description={appoinitment.Description}
+                    //          age={appoinitment.Age}
+                    //          contact={appoinitment.contact}
+                    //     />
+                    // ))
                 }
             </div>
         </div>
